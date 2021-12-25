647861db224d73df4cc50e7360e51b1b1dda41cf
harsh1507 <harshsingh15j@gmail.com>
2021-12-25 13:10:43 +0530

    ass-5_initial_commit

--- Assignment-5/ass-5/src/index.js
+++ Assignment-5/ass-5/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Postview from './Postview';
 import reportWebVitals from './reportWebVitals';
+
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Postview />
   </React.StrictMode>,
   document.getElementById('root')
 );
